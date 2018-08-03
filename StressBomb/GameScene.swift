928c0adbe2a65e0b9f2e0014e71dcdc0f7242eec
Herbeyg Robledo <herbeyg.robledo@g-tec.io>
2018-08-03 16:10:57 -0500

    solved issue that kept moving node when you were not clicking it

--- StressBomb/GameScene.swift
+++ StressBomb/GameScene.swift
@@ -12,30 +12,57 @@ import GameplayKit
 class GameScene: SKScene {
     
     var square = SKSpriteNode()
+    var circle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        square = self.childNode(withName: "Image") as! SKSpriteNode
+        square = self.childNode(withName: "square") as! SKSpriteNode
+        square.texture = SKTexture(imageNamed: "Square_figure_1")
         square.physicsBody?.affectedByGravity = true
+        
+        circle = self.childNode(withName: "circle") as! SKSpriteNode
+        circle.texture = SKTexture(imageNamed: "Circle_figure_1")
+        circle.physicsBody?.affectedByGravity = true
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
             let location = touch.location(in: self)
-            square.run(SKAction.moveTo(x: location.x, duration: 0.0))
-            square.run(SKAction.moveTo(y: location.y, duration: 0.0))
+            
+            if square.contains(location){
+                square.position = location
+            }
         }
     }
+    
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//        for touch in touches {
+//            let location = touch.location(in: self)
+//            square.run(SKAction.moveTo(x: location.x, duration: 0.0))
+//            square.run(SKAction.moveTo(y: location.y, duration: 0.0))
+//
+//            circle.run(SKAction.moveTo(x: location.x, duration: 0.0))
+//            circle.run(SKAction.moveTo(y: location.y, duration: 0.0))
+//        }
+//    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
             let location = touch.location(in: self)
-            square.run(SKAction.moveTo(x: location.x, duration: 0.0))
-            square.run(SKAction.moveTo(y: location.y, duration: 0.0))
             
+            if square.contains(location){
+                square.position = location
+            }
+            
+//            square.run(SKAction.moveTo(x: location.x, duration: 0.0))
+//            square.run(SKAction.moveTo(y: location.y, duration: 0.0))
+//
+//            circle.run(SKAction.moveTo(x: location.x, duration: 0.0))
+//            circle.run(SKAction.moveTo(y: location.y, duration: 0.0))
         }
     }
 }
-
-
