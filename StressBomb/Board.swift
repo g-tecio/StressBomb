a089abeef9eca49750d46d7c53016c1e3909a8f8
Alejandro <alejandro.solis@g-tec.io>
2018-08-10 15:10:24 -0500

    Added new assets

--- StressBomb/Board.swift
+++ StressBomb/Board.swift
@@ -33,35 +33,39 @@ extension Sequence {
 import SpriteKit
 
 struct ContainerSprite{
-        let block : SKSpriteNode!
-        let block2 : SKSpriteNode!
+    let block : SKSpriteNode!
+    let block2 : SKSpriteNode!
     
-    let containers = ["Circle_container_1","Moon_container_1","Square_container_1","Star_container_1","Triangle_container_1","Circle_container_2","Moon_container_2","Square_container_2","Star_container_2","Triangle_container_2","Circle_container_3","Moon_container_3","Square_container_3","Star_container_3","Triangle_container_3","Circle_container_4","Moon_container_4","Square_container_4","Star_container_4","Triangle_container_4","Circle_container_5","Moon_container_5","Square_container_5","Star_container_5","Triangle_container_5"]
+    let containers = ["Container_Circle_Blue","Container_Star_Blue","Container_Square_Blue","Container_Hexagone_Blue","Container_Triangle_Blue","Container_Circle_DBlue","Container_Hexagone_DBlue","Container_Square_DBlue","Container_Star_DBlue","Container_Triangle_DBlue","Container_Circle_Green","Container_Hexagone_Green","Container_Square_Green","Container_Star_Green","Container_Triangle_Green","Container_Circle_Red","Container_Hexagone_Red","Container_Square_Red","Container_Star_Red","Container_Triangle_Red","Container_Circle_Yellow","Container_Hexagone_Yellow","Container_Square_Yellow","Container_Star_Yellow","Container_Triangle_Yellow"]
     
-  
+    
     init (numContainer: Int, row: Int, col: Int , inThisScene: GameScene) {
         
         block = SKSpriteNode(imageNamed: containers[numContainer])
         block2 = SKSpriteNode(imageNamed: containers[1])
-        let numberOfBlocks = 10.5
+        let numberOfBlocks = 12.5
         let blockScale = 0.5
-       
+        
         // Resizing depending to screen size
         
         let blockOriginalSize = block.size
-      
         
-       // let blockWidth = block.size.width / 0.9
-       
-       
         
-        let blockWidth = block.size.width / 1.9
+        // let blockWidth = block.size.width / 0.9
+        
+        
+        
+        let blockWidth = block.size.width / 3.8
         let totalBlocksWidth = blockWidth * CGFloat(numberOfBlocks)
-        let xOffset = (inThisScene.size.width - totalBlocksWidth) / 2.70
+        let xOffset = (inThisScene.size.width - totalBlocksWidth) / 1.70
         
-//        block.position = CGPoint(x: (inThisScene.size.width * 0.225 * CGFloat(col)), y: (inThisScene.size.height * 0.825 * CGFloat(row)))
+        //        block.position = CGPoint(x: (inThisScene.size.width * 0.225 * CGFloat(col)), y: (inThisScene.size.height * 0.825 * CGFloat(row)))
         
-        block.position = CGPoint(x: xOffset + CGFloat( CGFloat(col) + 1) * (blockWidth * 1.480), y: (inThisScene.size.height * 1) - ((block.size.height * 0.780) * CGFloat(row)))
+        block.position = CGPoint(x: xOffset + CGFloat( CGFloat(col) + 1) * (blockWidth * 1.480), y: (inThisScene.size.height)  - ((block.size.height * 0.335) * CGFloat(row)) )
+        
+        
+        
+        block.size = CGSize(width: 105, height: 105)
         block.physicsBody = SKPhysicsBody(rectangleOf: block.frame.size)
         block.physicsBody!.collisionBitMask = 0
         block.physicsBody?.affectedByGravity = false
