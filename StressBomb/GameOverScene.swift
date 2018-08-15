dd61f3c57760532657bbf19d3b10109bc3b64dd3
Herbeyg Robledo <herbeyg.robledo@g-tec.io>
2018-08-15 16:56:35 -0500

    -Added Lauch Screen

---
+++ StressBomb/GameOverScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameOverScene.swift
+//  StressBomb
+//
+//  Created by Herbeyg Robledo Reyes on 8/15/18.
+//  Copyright © 2018 TeamLynx. All rights reserved.
+//
+
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+        override func didMove(to view: SKView) {
+            
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+
+}
---
+++ StressBomb/WinningScene.swift
@@ -0,0 +1,13 @@
+//
+//  WinningScene.swift
+//  StressBomb
+//
+//  Created by Herbeyg Robledo Reyes on 8/15/18.
+//  Copyright © 2018 TeamLynx. All rights reserved.
+//
+
+import SpriteKit
+
+class WinningScene: SKScene {
+
+}
